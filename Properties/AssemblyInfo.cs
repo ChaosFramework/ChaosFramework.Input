e6ea518fdbf61033255f7c44535483497d9997df
InsanityCode <56584762+InsanityCode@users.noreply.github.com>
2024-09-08 14:58:46 +0200

    add project

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ChaosFramework.Input")]
+[assembly: AssemblyProduct("ChaosFramework.Input")]
+[assembly: AssemblyCompany("ChaosTechnology")]
+[assembly: AssemblyCopyright("Copyright © ChaosTechnology 2024")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
